7f33c012eb1f1fde509286469e6f423dece3508d
Sati2030 <seigifabian@gmail.com>
2025-01-10 10:30:12 +0900

    Exit handling

--- app/main.c
+++ app/main.c
@@ -3,21 +3,23 @@
 
 int main() {
 
-  char input[100];
 
-  do{
+  while(1){
     // Flush after every printf
     setbuf(stdout, NULL);
     // Uncomment this block to pass the first stage
     printf("$ ");
     // Wait for user input
+    char input[100];
     fgets(input, 100, stdin);
     //Removes the new line of the input buffer
     input[strlen(input) - 1] = '\0';
+    if(strcmp(input,"exit 0") == 0){
+      break;
+    }
     //Prints (input command): command not found
     printf("%s: command not found\n",input);
   }
-  while(strcmp(input,"") != 0);
   
   return 0;
 }
