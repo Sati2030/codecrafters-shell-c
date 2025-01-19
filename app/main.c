03e85529a3f7c739cb57e948b730385210ad73ad
Sati2030 <seigifabian@gmail.com>
2025-01-19 20:55:56 +0900

    Finished type builtin with PATH

--- app/main.c
+++ app/main.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-typedef struct{
-  const char *name;
-  const char *type;
-} Command;
+const char *builtin[3] = {"exit","type","echo"};
 
-const Command commands[] = {
-  {"echo","builtin"},
-  {"exit","builtin"},
-  {"type","builtin"},
-  {NULL,NULL}
-};
-
-void echo(char *arg);
-void type(char *arg);
-void exit_(char *arg);
-Command valid_command(char *input);
-void command_handling(Command command,char *argument);
+void echo(char **arg,int num_args);
+void type(char **arg, int num_args);
+void exit_(char **arg, int num_args);
+char* valid_command(char *input);
+void command_handling(char **arguments,int num_args);
 
 
 int main() {
@@ -34,80 +25,176 @@ int main() {
     //Removes the new line of the input buffer
     input[strlen(input) - 1] = '\0';
 
-    //Returns a command if there is a valid command
-    Command command = valid_command(input);
+    //Creates a dynamic array of arguments
+    char *argum = strtok(input," ");
+    char **arguments = NULL;
+    int count = 0;
+    int length;
+  
+    while(argum){
+      arguments = realloc(arguments,(count+1) * sizeof(char *));
+      if(arguments == NULL){
+        printf("Memory allocation failed (arguments)\n");
+        exit(1);
+      }
+      
+      length = strlen(argum);
+
+      arguments[count] = malloc(length * sizeof(char));
+      if(arguments[count] == NULL){
+        printf("Memory allocation failed (argument)\n");
+        exit(1);
+      }
+
+      strcpy(arguments[count],argum);
+
+      count++;
+
+      argum = strtok(NULL, " ");
+    }  
+
+    char *temp = valid_command(arguments[0]);
 
     //Handles the commands
-    if(command.name != NULL){
-      char *argument = input + strlen(command.name) + 1;
-      command_handling(command,argument);
+    if(temp){
+      free(temp);
+      command_handling(arguments,count);
     }
     else{
-    //Prints (input command): command not found
-    printf("%s: command not found\n",input);
+      //Prints (input command): command not found
+      printf("%s: command not found\n",input);
     }
-  }
+
+    //Frees the dynamically allocated array of arguments
+    for(int i = 0; i < count; i++){
+      free(arguments[i]);
+    }
+    free(arguments);
   
+  }
   return 0;
 }
 
-//Checks if a command is valid and returns said command
-Command valid_command(char *input){
-  for(int i = 0; commands[i].name != NULL; i++){
 
-    size_t command_len = strlen(commands[i].name);
-    
-    if(strncmp(input, commands[i].name, command_len) == 0 ){
 
-      char following_char = input[command_len];
-      if(following_char == ' ' || following_char == '\0'){
-        return commands[i];
+//Checks if a command is valid and returns type
+char* valid_command(char *input){
+
+  //First check if the command is not a shell builtin
+  int builtinnum = sizeof(builtin)/sizeof(builtin[0]);
+  for(int i = 0; i<builtinnum;i++){
+    if(strcmp(builtin[i],input) == 0){
+      char *shell_builtin = (char *)malloc(14);
+      if(shell_builtin == NULL){
+        printf("Memory allocation failed (shell builtin type)\n");
+        exit(1);
       }
-
+      strcpy(shell_builtin,"shell builtin");
+      return shell_builtin;
     }
   }
-  return commands[sizeof(commands)/sizeof(commands[0]) -1];
+
+  //Retrieves the PATH 
+  const char *path_env = getenv("PATH");
+  if(path_env == NULL){
+    perror("Erorr retrieving PATH\n");
+    exit(1);
+  }
+
+  //Duplicates the PATH string
+  char *path = strdup(path_env);
+  if(path == NULL){
+    perror("There was an error duplicating the PATH\n");
+    exit(1);
+  }
+
+  //Tokenizes the Path
+  char *dir = strtok(path,":");
+
+  //Checking loop
+  while(dir != NULL){
+    
+    int len = strlen(dir) + strlen(input) + 2;
+    //Allocates memory for search
+    char *search = (char*)malloc(len);
+    if(search == NULL){
+      printf("Memory allocation failed (search in valid_command function)\n");
+      exit(1);
+    }
+
+    //Adds the path to search and the command
+    snprintf(search,len,"%s/%s",dir,input);
+
+    //If command is found in PATH directory return directory
+    if(!access(search,F_OK)){
+      free(path);
+      return(search);
+    }
+    else{
+      free(search);
+      dir = strtok(NULL,":");
+    }
+  }
+
+  free(path);
+  return NULL;
 }
 
 //Directs to its respecitve fucntion the command that is passed into the function
-void command_handling(Command command, char *argument){
-  if(strcmp(command.name,"echo") == 0){
-    echo(argument);
+void command_handling(char **arguments,int num_args){
+  if(strcmp(arguments[0],"echo") == 0){
+    echo(arguments,num_args);
   }
-  else if(strcmp(command.name,"type") == 0){
-    type(argument);
+  else if(strcmp(arguments[0],"type") == 0){
+    type(arguments,num_args);
   }
-  else if(strcmp(command.name,"exit") == 0){
-    exit_(argument);
-  }
+  else if(strcmp(arguments[0],"exit") == 0){
+      exit_(arguments,num_args);
+    }
 }
 
+
 //Function for the echo command
-void echo(char *arg){
-  printf("%s\n",arg);
+void echo(char **arg,int num_args){
+
+  for(int i = 1; i<num_args; i++){
+    printf("%s",arg[i]);
+    if(i == (num_args-1)){
+      printf("\n");
+    }
+    else{
+      printf(" ");
+    }
+  }
+
 }
 
 //Function for the type command
-void type(char *arg){
-  Command command = valid_command(arg);
-  if(command.name != NULL){
-    printf("%s is a shell %s\n",command.name,command.type);
-  }
-  else{
-    printf("%s: not found\n",arg);
+void type(char **arg, int num_args){
+
+  char *type;
+
+  for(int i = 1;i<num_args;i++){
+    type = valid_command(arg[i]);
+    if(type){
+      printf("%s is %s\n",arg[i],type);
+      free(type);
+    }
+    else{
+      printf("%s: not found\n",arg[i]);
+    }
   }
 }
 
 //Function for the exit command
-void exit_(char *arg){
+void exit_(char **arg, int num_args){
 
-  char *endptr;
-  long int value = strtol(arg, &endptr, 10);
-
-  if(*endptr != '\0' || value != 0){
-    printf("invalid argument\n");
-  }
-  else{
+  if(!strcmp(arg[1],"0")){
+    for(int i = 0; i<num_args;i++){
+      free(arg[i]);
+    }
+    free(arg);
     exit(0);
   }
+
 }
