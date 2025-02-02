4ec903bd856708dffbca10a8f804a2ab8879a254
Sati2030 <seigifabian@gmail.com>
2025-02-02 15:09:57 +0900

    Finished the implementation of autocompletion for PATH programs

--- app/main.c
+++ app/main.c
@@ -2,25 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <termios.h>
-
-const char *builtin[5] = {"exit","type","echo","pwd","cd"};
-int static count = 0;
-
-void echo(char **arg,int num_args);
-void type(char **arg, int num_args);
-void exit_(char **arg, int num_args);
-void cd(char **arg);
-void pwd();
-void deactivateCannonMode();
-char *readInput();
-char **redirection(char **arg);
-char **arg_arrayer(char *input);
-char *valid_command(char *input);
-void command_handling(char **arguments,int num_args);
-void program_execution(char **arg, char *prog);
-
+#include "input_reader.h"
+#include "argumenter.h"
+#include "commands.h"
+#include "redirection.h"
+#include "prog_interfacing.h"
 
 int main() {
 
@@ -39,40 +26,39 @@ int main() {
     //reads input
     char *input = readInput();
 
-    //Creates a dynamic array of argupments
-    char **args = arg_arrayer(input);
-    
+    //Creates a dynamic array of arguments
+    Arguments args = arg_arrayer(input);
+
     free(input);
 
     //Checks if valid command is passed to input
-    char *temp = valid_command(args[0]);
+    char *temp = valid_command(args.arguments[0]);
 
     //Handling of the redirection commands
-    args = redirection(args);
+    redirection(&args);
 
     //Handles the commands if shell builtins
     if(temp){
       if(!strcmp(temp,"a shell builtin")){
-        free(temp);
-        command_handling(args,count);
+        command_handling(&args);
       }
-      else if(args[0]){ //Else executes the program listed in the PATH variable
+      else if(strcmp(args.arguments[0],"\0")){ //Else executes the program listed in the PATH variable
         free(temp);
 
         //Adds NULL terminator to the argument array
-        args = realloc(args,(count+1)*sizeof(char*));
-        if(args == NULL){
+        args.arguments = realloc(args.arguments,(args.count+1)*sizeof(char*));
+        if(args.arguments == NULL){
           printf("Error reallocating memory for arguments array (NULL terminator)\n");
           exit(1);
         }
-        args[count] = NULL;
+        args.arguments[args.count] = NULL;
 
-        program_execution(args,args[0]);
+        program_execution(&args);
       }
     }
     else{
       //Prints (input command): command not found
-      printf("%s: command not found\n",args[0]);
+      printf("%s: command not found\n",args.arguments[0]);
     }
 
     //Bring the stdout back to the console
@@ -82,14 +68,15 @@ int main() {
     dup2(stderr_fd, fileno(stderr));
     close(stdout_fd);
     close(stderr_fd);
+    
 
     //Frees the dynamically allocated array of arguments
-    for(int i = 0; i < count; i++){
-      free(args[i]);
+    for(int i = 0; i < args.count; i++){
+      free(args.arguments[i]);
     }
-    free(args);
+    free(args.arguments);
 
-    count = 0;
+    args.count = 0;
   
   }
 
@@ -97,424 +84,4 @@ int main() {
 
 }
 
-char *readInput(){
 
-  char *input = NULL;
-  char c;
-  int i = 0;
-
-  while(read(STDIN_FILENO,&c,1) > 0){
-
-    if(c == '\n'){
-      input = realloc(input,(i+1)*sizeof(char));
-      printf("\n");
-      input[i] = '\0';
-      break;
-    }
-
-    if(c == 127){
-      if(i > 0){
-        input = realloc(input,(i)*sizeof(char));
-        printf("\b \b");
-        input[--i] = '\0';
-      }
-      continue;
-    }
-    
-
-    if(c == 9){
-      if(!strcmp(input,"ech")){
-        printf("o ");
-        input = realloc(input,(i+3)*sizeof(char));
-        input[i++] = 'o';
-        input[i++] = ' ';
-        input[i] = '\0';
-      }
-      else if(!strcmp(input,"exi")){
-        printf("t ");
-        input = realloc(input,(i+3)*sizeof(char));
-        input[i++] = 't';
-        input[i++] = ' ';
-        input[i] = '\0';
-      }
-      else if(!strcmp(input,"typ")){
-        printf("e ");
-        input = realloc(input,(i+3)*sizeof(char));
-        input[i++] = 'e';
-        input[i++] = ' ';
-        input[i] = '\0';
-      }
-      else{
-        printf("\a");
-      }
-      continue;
-    }
-
-
-    printf("%c",c);
-
-    input = realloc(input,(i+2)*sizeof(char));
-
-    input[i++] = c;
-    input[i] = '\0';
-
-  }
-
-  return input;
-}
-
-//Redirects stdout or stderr to a file if specified
-char **redirection(char **args){
-
-  //If there is a > then it directs the stdout or stderr to the desired file and removes from args list
-  for(int i = count-1; i >= 0; i--){
-    if(!strcmp(args[i],"2>")){  //If write stderr to a file
-      freopen(args[i+1],"w",stderr);
-      count -= 2;
-      free(args[i+1]);
-      free(args[i]);
-      args = realloc(args,count * sizeof(char *));
-    }
-    else if(!strcmp(args[i],">") || !strcmp(args[i],"1>")){  //If write stdout to a file
-      freopen(args[i+1],"w",stdout);
-      count -= 2;
-      free(args[i+1]);
-      free(args[i]);
-      args = realloc(args,count * sizeof(char *));
-    }
-    else if(!strcmp(args[i],">>") || !strcmp(args[i],"1>>")){ //If append stdout to a file
-      freopen(args[i+1],"a",stdout);
-      count-=2;
-      free(args[i+1]);
-      free(args[i]);
-      args = realloc(args,count*sizeof(char*));
-    }
-    else if(!strcmp(args[i],"2>>")){ //If append stderr to a file
-      freopen(args[i+1],"a",stderr);
-      count-=2;
-      free(args[i+1]);
-      free(args[i]);
-      args = realloc(args,count*sizeof(char*));
-    }
-  }
-
-  return(args);
-
-}
-
-//Organizes input into a dynamically allocated array
-char **arg_arrayer(char *input){
-  char **arguments = NULL;  //Arguments array
-  char buffer[1024];  //Temporary buffer to keep track of the argument placed
-  int i,j = 0;  //i = for the loop counter, j = for the position in the buffer
-  int sq_flag = 0; //Flag that activates if a single quote is opened
-  int dq_flag = 0; //Flag that activates if a double quote is opened
-  int bs_flag = 0; //Flag that activates if a backslash is opened
-
-  for(i = 0; i <= strlen(input); i++){
-
-    //Handling of single quotes
-    if(input[i] == '\''){
-      if(!dq_flag && !bs_flag){ //If not already in a double quote or backslash
-        sq_flag = !sq_flag;
-        continue; //Skips the quote in the buffer
-      }
-    }
-
-    //Handling of double quotes
-    if(input[i] == '"'){
-      if(!sq_flag && !bs_flag){ //If not already in single quotes or backslash
-        dq_flag = !dq_flag;
-        continue; //Skips the quote in the buffer 
-      } 
-    }
-
-    //Handling of backslashes
-    if(input[i] == '\\'){
-      if(dq_flag){  //If inside double quotes special meaning is reatained before another, " and $
-        if((input[i+1] == '\\' || input[i+1] == '"' || input[i+1] == '$') && input[i-1] != '\\'){
-          bs_flag = 1;
-          continue; //Skips the backslash in the buffer
-        }
-      }
-      else if(!sq_flag){ //Else if not inside single quotes
-        bs_flag = 1;
-        continue; //Skipts the backslash in the buffer
-      }
-    }
-
-    //Handling if there is multiple spaces in between arguments (but not inside quotation marks)
-    if(!sq_flag && !dq_flag && input[i] == ' ' && input[i-1] == ' '){
-      continue;
-    }
-
-    /*If not inside quotes, and not after a backslash, the space or '\0'
-    will store the buffer into an argument space*/
-    if(!sq_flag && !dq_flag && ((input[i] == ' ' && !bs_flag) || input[i] == '\0')){
-
-      buffer[j] = '\0';
-
-      arguments = realloc(arguments,(count +1)* sizeof(char *));
-      if(arguments == NULL){
-        printf("Memory allocation failed (arguments)\n");
-        exit(1);
-      }
-
-      arguments[count] = malloc((strlen(buffer) + 1) * sizeof(char));
-      if(arguments[count] == NULL){
-        printf("Memory allocation failed argument %d\n",count);
-        exit(1);
-      }
-
-      strcpy(arguments[count],buffer);
-
-      count++;
-      j = 0;
-      continue;
-    }
-
-    //Copy the input char in to the buffer
-    buffer[j++] = input[i];
-
-    //Makes sure backslash is deactivated after taking in the next character
-    bs_flag = 0;
-  }
-
-  return arguments;
-}
-
-//Checks if a command is valid and returns type
-char* valid_command(char *input){
-
-  //First check if the command is not a shell builtin
-  int builtinnum = sizeof(builtin)/sizeof(builtin[0]);
-  for(int i = 0; i<builtinnum;i++){
-
-    if(strcmp(builtin[i],input) == 0){
-      char *shell_builtin = (char *)malloc(16);
-      if(shell_builtin == NULL){
-        printf("Memory allocation failed (shell builtin type)\n");
-        exit(1);
-      }
-      strcpy(shell_builtin,"a shell builtin");
-      return shell_builtin;
-    }
-  }
-
-
-  //Retrieves the PATH 
-  const char *path_env = getenv("PATH");
-  if(path_env == NULL){
-    perror("Erorr retrieving PATH\n");
-    exit(1);
-  }
-
-  //Duplicates the PATH string
-  char *path = strdup(path_env);
-  if(path == NULL){
-    perror("There was an error duplicating the PATH\n");
-    exit(1);
-  }
-
-  //Tokenizes the Path
-  char *dir = strtok(path,":");
-
-  //Checking loop
-  while(dir != NULL){
-    
-    int len = strlen(dir) + strlen(input) + 2;
-    //Allocates memory for search
-    char *search = (char*)malloc(len);
-    if(search == NULL){
-      printf("Memory allocation failed (search in valid_command function)\n");
-      exit(1);
-    }
-
-    //Adds the path to search and the command
-    snprintf(search,len,"%s/%s",dir,input);
-
-    //If command is found in PATH directory return directory
-    if(!access(search,F_OK)){
-      free(path);
-      return(search);
-    }
-    else{
-      free(search);
-      dir = strtok(NULL,":");
-    }
-  }
-
-  free(path);
-  return NULL;
-}
-
-//Directs to its respecitve fucntion the command that is passed into the function
-void command_handling(char **arguments,int num_args){
-  if(!strcmp(arguments[0],"echo")){
-    echo(arguments,num_args);
-  }
-  else if(!strcmp(arguments[0],"type")){
-    type(arguments,num_args);
-  }
-  else if(!strcmp(arguments[0],"exit")){
-    exit_(arguments,num_args);
-    }
-  else if(!strcmp(arguments[0],"pwd")){
-    pwd();
-  }
-  else if(!strcmp(arguments[0],"cd")){
-    cd(arguments);
-  }
-
-  return;
-}
-
-//Executes any program that is listed in the PATH variable
-void program_execution(char **arg, char *prog){
-  
-  //Forks a new process to execute program in 
-  pid_t pr = fork();
-
-  switch (pr)
-  {
-    //If child process
-    case 0:
-      fflush(stdout);
-      if(execvp(prog,arg) == -1){
-        printf("Error executing program \n");
-        _exit(1);
-      }
-      break;
-    //If fork fails
-    case -1:
-      printf("Fork failed\n");
-      break;
-    //If parent process
-    default:
-      wait(NULL);
-      break;
-  }
-
-  return;
-}
-
-//Deactivates cannonical mode for instant input reading
-void deactivateCannonMode(){
-  
-  struct termios terminal;
-
-  if(tcgetattr(STDIN_FILENO, &terminal) == -1){
-    printf("Error getting terminal attributes\n");
-    exit(1);
-  }
-
-  terminal.c_lflag &= ~(ICANON|ECHO|ISIG);
-
-  if(tcsetattr(STDIN_FILENO,TCSANOW,&terminal) == -1){
-    printf("Error setting terminal attributes\n");
-    exit(1);
-  }
-
-  return;
-
-}
-
-//Function for the cd command
-void cd(char **arg){
-   
-  //If there is no argument or the argument is "~" go to the HOME directory
-  if(!arg[1] || !strcmp(arg[1],"~")){
-    if(!chdir(getenv("HOME"))){
-      return;
-    }
-    else{
-      printf("Error changing to home directory\n");
-      return;
-    }
-  }
-  //Else move to the specified directory
-  else if(!chdir(arg[1])){
-    return;
-  }
-  //If specified directory does not exist
-  else{
-    printf("cd: %s: No such file or directory\n",arg[1]);
-  }
-
-  return;
-}
-
-//Function for the pwd command
-void pwd(){
-  char *cwd = getcwd(NULL,0);
-
-  if(cwd){
-    printf("%s\n",cwd);
-    free(cwd);
-  }
-  else{
-    printf("Error locating current working directory\n");
-  }
-
-  return;
-}
-
-//Function for the echo command
-void echo(char **arg,int num_args){
-
-  for(int i = 1; i<(num_args); i++){
-    printf("%s",arg[i]);
-    if(i == (num_args-1)){
-      printf("\n");
-    }
-    else{
-      printf(" ");
-    }
-  }
-
-  return;
-
-}
-
-//Function for the type command
-void type(char **arg, int num_args){
-
-  char *type;
-
-  for(int i = 1;i<num_args;i++){
-    type = valid_command(arg[i]);
-    if(type){
-      printf("%s is %s\n",arg[i],type);
-      free(type);
-    }
-    else{
-      printf("%s: not found\n",arg[i]);
-    }
-  }
-
-  return;
-}
-
-//Function for the exit command
-void exit_(char **arg, int num_args){
-
-  //Handles if there is just exit
-  if(num_args < 2){
-    printf("Not enough arguments\n");
-    return;
-  }
-
-  if(!strcmp(arg[1],"0")){
-    for(int i = 0; i<num_args;i++){
-      free(arg[i]);
-    }
-    free(arg);
-    exit(0);
-  }
-  else{
-    printf("Not a valid argument\n");
-    return;
-  }
-
-  return;
-}
